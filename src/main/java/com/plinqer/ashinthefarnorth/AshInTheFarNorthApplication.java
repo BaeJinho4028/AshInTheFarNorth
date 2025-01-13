a58c245458dba135da2fa9c5a54dff4563343de5
Baejinho4028 <onionring4028@naver.com>
2025-01-13 11:57:42 +0900

    feat: 기본 세팅

---
+++ src/main/java/com/plinqer/ashinthefarnorth/AshInTheFarNorthApplication.java
@@ -0,0 +1,13 @@
+package com.plinqer.ashinthefarnorth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshInTheFarNorthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AshInTheFarNorthApplication.class, args);
+    }
+
+}
