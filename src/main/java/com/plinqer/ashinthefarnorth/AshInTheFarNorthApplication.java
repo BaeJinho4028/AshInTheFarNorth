e3eaa65a2bf52ab8bbff596c149e7fb78d6f508e
Baejinho4028 <onionring4028@naver.com>
2025-01-13 13:49:58 +0900

    feat: .gitignore 추가

--- src/main/java/com/plinqer/ashinthefarnorth/AshInTheFarNorthApplication.java
+++ src/main/java/com/plinqer/ashinthefarnorth/AshInTheFarNorthApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AshInTheFarNorthApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(AshInTheFarNorthApplication.class, args);
     }
-
 }
