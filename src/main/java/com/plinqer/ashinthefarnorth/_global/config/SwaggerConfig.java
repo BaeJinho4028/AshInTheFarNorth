88588c0d39256798bbe5a3bc0737c0e295678fb3
Baejinho4028 <onionring4028@naver.com>
2025-01-15 18:21:00 +0900

    fix: swagger server url 수정

--- src/main/java/com/plinqer/ashinthefarnorth/_global/config/SwaggerConfig.java
+++ src/main/java/com/plinqer/ashinthefarnorth/_global/config/SwaggerConfig.java
@@ -1,18 +1,29 @@
 package com.plinqer.ashinthefarnorth._global.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 
 @Configuration
 public class SwaggerConfig {
 
+    private final String serverUrl;
+
+    public SwaggerConfig(
+        @Value("${springdoc.server.url}") String serverUrl
+    ) {
+        this.serverUrl = serverUrl;
+    }
+
     @Bean
     public OpenAPI springShopOpenAPI() {
         return new OpenAPI()
-            .info(apiInfo());
+            .info(apiInfo())
+            .addServersItem(apiServer());
     }
 
     private Info apiInfo() {
@@ -20,5 +31,10 @@ public class SwaggerConfig {
             .title("AshInTheFarNorth API")
             .description("최북단 사는 최재애쉬 API 문서입니다.")
             .version("v0.0.1");
+    }
+
+    private Server apiServer() {
+        return new Server()
+            .url(serverUrl);
     }
 }
